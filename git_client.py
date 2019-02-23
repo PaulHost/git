d1eff82b8f860343d23a12a07472195661cf9fde
Paul Hostev <paul.hostev@gmail.com>
2019-02-24 00:19:56 +0300

    [master] - Global changing author name and email + refactoring

--- git_client.py
+++ git_client.py
@@ -3,6 +3,7 @@
 import sys
 from src import GitClient
 
+
 def main(argv):
     print(argv)
     if len(argv) == 1:
@@ -10,6 +11,7 @@ def main(argv):
             change email on each commit: -e old_mail new_mail\n\
             change committer name on each commit: -n "old name" "new name"\n\
             change email and name: -e -n old_mail new_mail "old name" "new name"\n\
+                -g renames or changed email global\n\
             rename branch: -r old_branch_name new_branch_name')
     else:
         client = GitClient()
@@ -17,18 +19,25 @@ def main(argv):
         if first_arg == '-e':
             second_arg = argv.pop(0)
             if second_arg == '-n':
-                client.change_email_and_name(old_email=argv.pop(0),\
-                                             new_email=argv.pop(0),\
-                                             old_name=argv.pop(0),\
-                                             new_name=argv.pop(0))
+                client.change_email_and_author(old_email=argv.pop(0), \
+                                               new_email=argv.pop(0), \
+                                               old_name=argv.pop(0), \
+                                               new_name=argv.pop(0), \
+                                               g=isGlobal(argv))
             else:
-                client.change_email(second_arg, argv.pop(0))
+                client.change_email(second_arg, argv.pop(0), g=isGlobal(argv))
         elif first_arg == '-n':
-            client.author_renaming(old_name=argv.pop(0), new_name=argv.pop(0))
-        elif  first_arg == '-r':
+            client.author_renaming(old_name=argv.pop(0), new_name=argv.pop(0), g=isGlobal(argv))
+        elif first_arg == '-r':
             client.rename_branch(old_name=argv.pop(0), new_name=argv.pop(0))
 
 
+def isGlobal(argv):
+    try:
+        return argv.pop(0) == '-g'
+    except IndexError:
+        return False
+
 
 if __name__ == '__main__':
-   main(sys.argv[1:])
+    main(sys.argv[1:])
--- src/gitclient.py
+++ src/gitclient.py
@@ -1,13 +1,16 @@
 import os
 from subprocess import call as cmd
 
+
 class GitClient(object):
     def __init__(self):
         self.sh = 'sh'
-        self.package = os.path.dirname(__file__)
+        self.package = os.path.dirname(__file__) + '/sh_scripts'
         self.author_renaming_script = self.package + '/author_renaming.sh'
         self.change_email_script = self.package + '/change_email.sh'
         self.rename_branch_script = self.package + '/rename_branch.sh'
+        self.rename_global_script = self.package + '/rename_global.sh'
+        self.change_email_global_script = self.package + '/change_email_global.sh'
 
     def author_renaming_command(self, old_name, new_name):
         return [self.sh, self.author_renaming_script, old_name, new_name]
@@ -18,16 +21,37 @@ class GitClient(object):
     def rename_branch_command(self, old_name, new_name):
         return [self.sh, self.rename_branch_script, old_name, new_name]
 
-    def change_email(self, old_email, new_email):
-        cmd(self.change_email_command(old_email, new_email))
+    def rename_global_command(self, new_global_name):
+        return [self.sh, self.rename_branch_script, new_global_name]
 
-    def author_renaming(self, old_name, new_name):
+    def change_email_global_command(self, new_global_email):
+        return [self.sh, self.change_email_global_script, new_global_email]
+
+    def change_email_global(self, new_global_email):
+        cmd(self.change_email_global_command(new_global_email))
+
+    def rename_global(self, new_global_name):
+        cmd(self.rename_global_command(new_global_name))
+
+    def change_email(self, old_email, new_email, g=False):
+        cmd(self.change_email_command(old_email, new_email))
+        if g:
+            self.change_email_global(new_email)
+
+    def author_renaming(self, old_name, new_name, g=False):
         cmd(self.author_renaming_command(old_name, new_name))
+        if g:
+            self.rename_global_command(new_name)
+
+    def rename_and_change_email_global(self, new_email, new_name):
+        self.change_email_global(new_email)
+        self.rename_global(new_name)
+
+    def change_email_and_author(self, old_email, new_email, old_name, new_name, g=False):
+        self.change_email(old_email, new_email)
+        self.author_renaming(old_name, new_name)
+        if g:
+            self.rename_and_change_email_global(new_email, new_name)
 
     def rename_branch(self, old_name, new_name):
         cmd(self.rename_branch_command(old_name, new_name))
-
-    def change_email_and_name(self, old_email, new_email, old_name, new_name):
-        self.change_email(old_email, new_email)
-        self.author_renaming(old_name, new_name)
-
